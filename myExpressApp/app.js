fb99d0cfd8422eb1a5779ed79f9359bf300cb104
liora <liora_levy_1@hotmail.fr>
2025-09-07 10:41:18 +0300

    update app.js

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/test', (req, res) => {
+  res.send('Nouvelle version de l’app !');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
